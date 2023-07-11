c6a0cb16d425b8b65acc1cd186be9060d1476f43
Jake Armour <github@jakearmour.com>
2023-07-11 20:00:35 +0100

    static data in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
-function App() {
-  return <p>Hello</p>;
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
-export default App;
+const Logo = () => {
+  return <h1>🏝️ Far Away 🧳</h1>;
+};
+
+const Form = () => {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  );
+};
+
+const PackingList = () => {
+  return <div className="list">list</div>;
+};
+
+const Stats = () => {
+  return (
+    <footer className="stats">
+      <em>💼 You have X items on your list, and you have packed X (X%)</em>
+    </footer>
+  );
+};
