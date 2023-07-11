1924ef282b5892affb1a460204120b37ec601da4
Jake Armour <github@jakearmour.com>
2023-07-11 20:08:42 +0100

    item list initial work with static data

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -22,13 +27,37 @@ const Form = () => {
 };
 
 const PackingList = () => {
-  return <div className="list">list</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((i) => (
+          <Item key={i.id} item={i} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const Item = ({ item }) => {
+  const { id, description, quantity, packed } = item;
+
+  return (
+    <li>
+      <span style={packed ? { textDecoration: "line-through" } : {}}>
+        {quantity} {description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 };
 
 const Stats = () => {
   return (
     <footer className="stats">
-      <em>💼 You have X items on your list, and you have packed X (X%)</em>
+      <em>
+        💼 You have {initialItems.length} items on your list, and you have
+        packed X (X%)
+      </em>
     </footer>
   );
 };
