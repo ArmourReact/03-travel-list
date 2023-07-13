9d14c8a4482750b6ad00bf8448c328a0fae5831c
Jake Armour <github@jakearmour.com>
2023-07-13 11:37:06 +0100

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -117,6 +117,12 @@ const Item = ({ item, onDeleteItem, onToggleItem }) => {
 };
 
 const Stats = ({ items }) => {
+  if (!items.length)
+    return (
+      <footer className="stats">
+        <em>Start adding items to your packing list</em>
+      </footer>
+    );
   const totalItems = items.length;
   const packedItems = items.filter((i) => i.packed).length;
   const packedPercent = Math.round((packedItems / totalItems) * 100);
@@ -124,8 +130,10 @@ const Stats = ({ items }) => {
   return (
     <footer className="stats">
       <em>
-        💼 You have {totalItems} items on your list, and you have packed{" "}
-        {packedItems} ({packedPercent}%)
+        {packedPercent === 100
+          ? "You got everything! Ready to go ✈️"
+          : `💼 You have ${totalItems} items on your list, and you have packed 
+        ${packedItems} (${packedPercent}%)`}
       </em>
     </footer>
   );
